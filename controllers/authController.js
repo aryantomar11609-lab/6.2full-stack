93baf863730a10c7e8e3cfe2af69b22a6c2de542
aryantomar11609-lab <aryantomar11609@gmail.com>
2025-10-31 10:38:46 +0530

    Create authController.js

---
+++ controllers/authController.js
@@ -0,0 +1,14 @@
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../config/keys');
+const user = require('../models/userModel');
+
+exports.login = (req, res) => {
+  const { username, password } = req.body;
+
+  if (username === user.username && password === user.password) {
+    const token = jwt.sign({ username }, SECRET_KEY, { expiresIn: '1h' });
+    res.status(200).json({ token });
+  } else {
+    res.status(401).json({ message: 'Invalid credentials' });
+  }
+};
