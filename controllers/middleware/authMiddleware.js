05ab99ef05ce1c4f8fff3423d0b7a8550dff4866
aryantomar11609-lab <aryantomar11609@gmail.com>
2025-10-31 10:40:01 +0530

    Create authMiddleware.js

---
+++ controllers/middleware/authMiddleware.js
@@ -0,0 +1,20 @@
+const jwt = require('jsonwebtoken');
+const { SECRET_KEY } = require('../config/keys');
+
+module.exports = (req, res, next) => {
+  const authHeader = req.headers['authorization'];
+
+  if (!authHeader || !authHeader.startsWith('Bearer ')) {
+    return res.status(401).json({ message: 'Authorization header missing or incorrect' });
+  }
+
+  const token = authHeader.split(' ')[1];
+
+  jwt.verify(token, SECRET_KEY, (err, decoded) => {
+    if (err) {
+      return res.status(403).json({ message: 'Invalid or expired token' });
+    }
+    req.user = decoded;
+    next();
+  });
+};
